6638a1c61e322ac24bd9f8f54adf83cd0c1a3536
Yeongheun <iyoungheuni@gmail.com>
2025-06-06 22:22:58 +0900

    feat: Add chat'

---
+++ lib/services/mission_api.dart
@@ -0,0 +1,18 @@
+// lib/services/mission_api.dart
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class MissionApi {
+  static const String baseUrl = 'http://172.30.1.3:8080'; // 예: http://10.0.2.2:8080
+
+  static Future<List<Map<String, dynamic>>> fetchMissions() async {
+    final response = await http.get(Uri.parse('$baseUrl/missions'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonList = json.decode(utf8.decode(response.bodyBytes));
+      return jsonList.map((e) => e as Map<String, dynamic>).toList();
+    } else {
+      throw Exception('게시글 불러오기 실패: ${response.statusCode}');
+    }
+  }
+}
